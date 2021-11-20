83fcc00dc3c0e824eba446239f185de8ed8748d2
Julian Jeremiah Weske <jweske@clabsql.clamv.jacobs-university.de>
2021-11-20 20:28:30 +0100

    added function to get audioFeatures

--- server/server.py
+++ server/server.py
@@ -11,14 +11,17 @@ from spotipy.oauth2 import SpotifyClientCredentials
 
 app = Flask(__name__)
 cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route("/")
 def hello_world():
     return render_template('index.html')
 
 @app.route("/search", methods=['POST'])
+@cross_origin()
 def search():
-    search = request.form["search"]
+    search = request.json
+    print (search)
     cid = '4bb4bbafa39540db902e05b2e0d60b6a'
     secret = 'be8a2b6a8d554bacac4f8117db6e2ee4'
     client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
@@ -31,27 +34,27 @@ def search():
 
     file = open("output.txt", 'w')
 
-    track_results = sp.search(q=search, type='track', limit=10)
+    track_results = sp.search(q=search["search"], type='track', limit=5)
     for i, t in enumerate(track_results['tracks']['items']):
         artist_name.append(t['artists'][0]['name'])
         track_name.append(t['name'])
         track_id.append(t['id'])
         popularity.append(t['popularity'])
 
-
     file.write(json.dumps(track_results))
 
-    for i in range(0,10):
-        if(track_name[i] == "All I Want for Christmas Is You"):
-            file.write("Fuck you\n")
-        else:
-            file.write(json.dumps(artist_name[i]))
-            file.write(json.dumps(track_name[i]))
-            file.write(json.dumps(popularity[i]))
-            file.write(json.dumps(track_id[i]))
-            file.write("\n")
+    songs = []
 
-    return "got the gest request"
+    for i in range(0,5):
+        song = {
+            "artist" : artist_name[i],
+            "track_name" :  track_name[i],
+            "track_id" : track_id[i],
+            "popularity" : popularity[i]
+        }
+        songs.append(song)
+
+    return json.dumps(songs)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0",debug=True)
